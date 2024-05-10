3181c30a4eedd66e3cc106171920135d70d6c7b8
longle11 <longleee2003@gmail.com>
2024-05-10 22:24:33 +0700

    update package with express-validation and validation for create api

--- Issue/src/Routes/create.js
+++ Issue/src/Routes/create.js
@@ -4,36 +4,54 @@ const currentUserMiddleware = require("../Middlewares/currentUser-Middleware")
 const issuePublisher = require("../nats/publisher/issue-publisher")
 const BadRequestError = require("../Errors/Bad-Request-Error")
 const UnauthorizedError = require("../Errors/UnAuthorized-Error")
+const { check, validationResult } = require('express-validator');
+
 
 const router = express.Router()
 
-router.post("/create", currentUserMiddleware, async (req, res, next) => {
+router.post("/create", currentUserMiddleware, [
+    check('shortSummary')
+        .isEmpty().withMessage("shortSummary is not empty")
+        .isLength({ min: 5 }).withMessage('must be at least 5 characters long'),
+    check('timeSpent')
+        .isInt({ min: 1 }).withMessage("timeSpent must be a integer number and greater than or equal to 1"),
+    check('timeRemaining')
+        .isInt({ min: 1 }).withMessage("timeRemaining must be a integer number and greater than or equal to 1"),
+    check('timeOriginalEstimate')
+        .isInt({ min: 1 }).withMessage("timeOriginalEstimate must be a integer number and greater than or equal to 1")
+], async (req, res, next) => {
     try {
         if (req.currentUser) {
-            const currentIssue = await issueModel.findOne({ shortSummary: req.body.shortSummary }).exec()
-            if (currentIssue === null) {
-                const newIssue = await issueModel.create(req.body)
-
-                const issueCopy = {
-                    _id: newIssue._id,
-                    projectId: newIssue.projectId,
-                    priority: newIssue.priority,
-                    shortSummary: newIssue.shortSummary,
-                    positionList: newIssue.positionList,
-                    issueType: newIssue.issueType,
-                    issueStatus: newIssue.issueStatus,
-                    assignees: newIssue.assignees,
-                    creator: newIssue.creator
-                }
-
-                await issuePublisher(issueCopy, 'issue:created')
-
-                return res.status(201).json({
-                    message: "Successfully created an issue",
-                    data: newIssue
-                })
+            const result = validationResult(req);
+            if (!result.isEmpty()) {
+                throw new BadRequestError('Information is invalid')
             } else {
-                throw new BadRequestError("Short summary field is already existed")
+                const { shortSummary } = req.body
+                const currentIssue = await issueModel.findOne({ shortSummary })
+                if (currentIssue === null) {
+                    const newIssue = await issueModel.create(req.body)
+
+                    const issueCopy = {
+                        _id: newIssue._id,
+                        projectId: newIssue.projectId,
+                        priority: newIssue.priority,
+                        shortSummary: newIssue.shortSummary,
+                        positionList: newIssue.positionList,
+                        issueType: newIssue.issueType,
+                        issueStatus: newIssue.issueStatus,
+                        assignees: newIssue.assignees,
+                        creator: newIssue.creator
+                    }
+
+                    await issuePublisher(issueCopy, 'issue:created')
+
+                    return res.status(201).json({
+                        message: "Successfully created an issue",
+                        data: newIssue
+                    })
+                } else {
+                    throw new BadRequestError("Short summary field is already existed")
+                }
             }
         } else {
             throw new UnauthorizedError("Authentication failed")
