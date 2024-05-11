fade18a883f71bf1cce30c146ea179cd7e573396
longle11 <longleee2003@gmail.com>
2024-05-11 21:20:29 +0700

    update client newest

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,3 @@
-import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { UserLoginTemplate } from './templates/UserLoginTemplate';
 import Login from './components/Login/Login';
