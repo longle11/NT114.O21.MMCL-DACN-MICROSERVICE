34af0fa20c8e9cc00423395b9e936eaf1e39721c
longle11 <longleee2003@gmail.com>
2024-05-11 14:34:00 +0700

    update client 12

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -127,9 +127,9 @@ export default function Dashboard() {
             .sort((issue1, issue2) => issue1.priority - issue2.priority)
             .map((value, index) => {
                 return (<li key={value._id} className="list-group-item" data-toggle="modal" data-target="#infoModal" style={{ cursor: 'pointer' }}>
-                    <div onClick={() => {
+                    <div role='button' onClick={() => {
                         dispatch(getInfoIssue(value._id))
-                    }} onKeyDown={() => {}} tabIndex={-42}>
+                    }} onKeyDown={() => {}}>
                         {value.shortSummary}
                     </div>
                     <div className="block" style={{ display: 'flex' }}>
